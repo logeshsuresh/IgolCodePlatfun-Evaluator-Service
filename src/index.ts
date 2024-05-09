2919907d797f186b30a0c311ee8308322106035c
Logesh Suresh <logeshsrinivasans@gmail.com>
2024-05-09 21:30:15 +0530

    Added BullBoard & BullBoard UI

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 import express, { Express } from "express";
 
+import bullBoardAdapter from "./config/bullBoardConfig";
 import serverConfig from "./config/serverConfig";
 import sampleQueueProducer from "./producers/sampleQueueProducer";
 import apiRouter from "./routes";
@@ -8,14 +9,16 @@ import SampleWorker from "./workers/sampleWorker";
 const app: Express = express();
 
 app.use('/api', apiRouter);
+app.use('/ui', bullBoardAdapter.getRouter());
 
 app.listen(serverConfig.PORT, async () => {
   console.log(`Server started at PORT ${serverConfig.PORT}`);
+  console.log(`BullBoard dashboard running on: http://localhost:${serverConfig.PORT}/ui`);
   SampleWorker("SampleQueue");
   sampleQueueProducer("SampleJob", {
     name: "Logesh",
     company: "Searce",
     position: "SDE-1",
     location: "BLR"
-  });
+  }, 1);
 });
